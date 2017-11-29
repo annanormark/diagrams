f52a599d3a62f8775ab92fcab86da25a172daa0a
marcusfroling <colamackan@hotmail.com>
2017-11-29 17:44:18 +0100

    kod

---
+++ kod/Response.java
@@ -0,0 +1,16 @@
+import kod.Request;
+
+public class Response{
+    private Request request;
+    private boolean ready;
+    private boolean isAccepted;
+    private int resonderID;
+
+    public void cancel(){
+
+    }
+    public void isReady(){
+
+    }
+
+}
--- kod/User.java
+++ kod/User.java
@@ -1,4 +1,3 @@
-
 import .AccountManagement;
 
 public class User {
